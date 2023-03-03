661912a39492bc7b7f5ca60db09c73bc38d52f8e
imruhi <61055561+imruhi@users.noreply.github.com>
2023-03-03 17:09:41 +0100

    extracting dataset

--- german_data/loader.py
+++ german_data/loader.py
@@ -1,14 +1,11 @@
 from datasets import load_dataset
-import os
-
 
 def load_data():
     dataset = load_dataset("csv", data_files={"train": ["deu_100.tsv", "deu_200.tsv", "deu_300.tsv", 
                                                         "deu_400.tsv", "deu_500.tsv", "deu_600.tsv"],
                                               "test": "deu_gold.tsv", 
                                               "validation": "deu_dev.tsv"})
-
-    # 
+ 
     return dataset
 
 if __name__ == "__main__":
