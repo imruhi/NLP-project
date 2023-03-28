39e83d36c084b4811cb680045239665e294314be
imruhi <61055561+imruhi@users.noreply.github.com>
2023-03-28 19:00:21 +0200

    Add files via upload

--- inseq_analysis/analysis.py
+++ inseq_analysis/analysis.py
@@ -1,4 +1,9 @@
 import inseq
+import pandas as pd
+import tensorflow as tf
+import torch 
+import numpy as np
+from inseq.data.aggregator import AggregatorPipeline, ContiguousSpanAggregator, SequenceAttributionAggregator, PairAggregator
 
 def do_contrast_analysis(model_filepath, input_text, contrast_text, truth_text):
     """ Do contrastive analysis as shown in W5E notebook  
@@ -23,15 +28,33 @@ def do_contrast_analysis(model_filepath, input_text, contrast_text, truth_text):
     out.weight_attributions("contrast_prob_diff")
     out.show()
 
+    # TODO challenge part
+
 def do_analysis(model_filepath, input_text):
     """ Do analysis of the prediction (unconstrained) as in W5E notebook
     """
-    # not very good probably because of lack of training data?
-    # possibly could be because we're not tokenizing the tags and lemma correctly but don't know yet
     model = inseq.load_model(model_filepath, "input_x_gradient")
-    out = model.attribute(
+    out1 = model.attribute(
         input_text,
         attribute_target=True,
         step_scores=["probability"]
     )
-    out.show()
+
+    # TODO: challange part
+    source = out1.sequence_attributions[0].source
+    target = out1.sequence_attributions[0].target
+
+    squeezesum = AggregatorPipeline([ContiguousSpanAggregator, SequenceAttributionAggregator])
+
+    # Aggregate outputs to get the tables
+    agg = out1.sequence_attributions[0].aggregate(aggregator=SequenceAttributionAggregator)
+    source_att = agg.source_attributions
+    target_att = target_attributions
+    out1.show()
+
+    print(source)
+    print(source_att)
+    print()
+    print(target)
+    print(target_att)
+    # N;NOM;FEM;PL: Orgie
