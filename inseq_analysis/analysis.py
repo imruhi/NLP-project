674dbb53c7cd368500887e118ffa0b931e633874
imruhi <61055561+imruhi@users.noreply.github.com>
2023-03-30 23:01:29 +0200

    added challenge part

--- inseq_analysis/analysis.py
+++ inseq_analysis/analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 import tensorflow as tf
 import torch 
 import numpy as np
-from inseq.data.aggregator import AggregatorPipeline, SequenceAttributionAggregator
+from inseq.data.aggregator import SequenceAttributionAggregator
 import re
 
 def do_contrast_analysis(model_filepath, input_text, contrast_text, truth_text):
@@ -32,6 +32,7 @@ def do_contrast_analysis(model_filepath, input_text, contrast_text, truth_text):
 def do_analysis(model_filepath, input_text):
     """ Do analysis of the prediction (unconstrained) as in W5E notebook
     """
+    print(f"#### Doing analysis at model in filepath {model_filepath} ####")
     model = inseq.load_model(model_filepath, "input_x_gradient")
     out = model.attribute(
         input_text,
@@ -39,64 +40,77 @@ def do_analysis(model_filepath, input_text):
         step_scores=["probability"]
     )
 
-    out.show()
+    out.show(return_html=True)
 
-def get_average_score(model_f, preds_f, labels_f, test_f, bCorrect = True):
+def get_average_score(model_f, test_f, bCorrect = True):
     """ TODO: challenge part, get the average attribution scores given a prediction list of
     plural nouns 
     :param bCorrect: if True get scores for correct predictions else get scores for incorrect predictions 
     """  
+    pattern1 = ";FEM;|;MASC;|;NEUT;"    # gender              
 
     # get all data needed
     print("#### Getting data ####")
-    my_file = open(labels_f, "r")
-    labels = my_file.read().split(',')
-    my_file.close()
+    df = pd.read_csv(test_f, low_memory = True)
 
-    my_file = open(preds_f, "r")
-    outputs = my_file.read().split(',')
-    my_file.close()
-
-    df = pd.read_csv(test_f, usecols = ['tag','lemma'], low_memory = True)
     tags = df['tag'].values.tolist()
     lemma = df['lemma'].values.tolist()
+
+    labels = df["target_form"].values.tolist()
 
     inputs = []
     for i in range(len(tags)):
         inputs.append(tags[i] + ": " + lemma[i])
 
-    # TODO: challange part
+    # Challange part
     model = inseq.load_model(model_f, "input_x_gradient")
 
     print("#### Attributing ####")
-    out = model.attribute(
-        inputs,
-        attribute_target=True,
-        step_scores=["probability"]
-    )
+    extract_atts_fem = []
+    extract_atts_masc = []
+    extract_atts_neut = []
 
-    # get first source and target from preds
-    attribution = out.sequence_attributions[0]
-    source = ''.join([t.token for t in attribution.source])
-    target = ''.join([t.token for t in attribution.target])
+    print(len(inputs))
 
-    squeezesum = AggregatorPipeline([SequenceAttributionAggregator])
+    for i in range(len(inputs)):
 
-    # Aggregate outputs to get the values from the heatmaps (first pred)
-    # round up to 2 decimal places
-    agg = out.sequence_attributions[0].aggregate(aggregator=SequenceAttributionAggregator)
-    source_att = np.around(agg.source_attributions.numpy(), 2)
-    target_att = np.around(agg.target_attributions.numpy(), 2)
-    out.show()
+        # get attribution scores
+        out = model.attribute(
+            inputs[i],
+            attribute_target=True,
+            step_scores=["probability"]
+        )   
+        print(i)
+        attribution = out.sequence_attributions[0]
+        target = ''.join([t.token for t in attribution.target][:-1])    # ignore eos token
 
-    pattern1 = ";FEM;|;MASC;|;NEUT;"    # gender
-    pattern2 = ";N;"                    # pos
-    pattern3 = ";NOM;"                  # case
-    pattern4 = ";PL: "                  # number
-    
-    start_idx, end_idx = re.search(pattern1, source).span()
-    extract_att = source_att[np.arange(start_idx, end_idx), :]
-    
-    # extract the section near the ;GENDER; area
-    print(extract_att)  
-    # N;NOM;FEM;PL: Orgie
+        if (target == labels[i]) and bCorrect:
+            start_idx, end_idx = re.search(pattern1, inputs[i]).span()
+            # Aggregate scores to get the values from the heatmaps
+            agg = out.sequence_attributions[0].aggregate(aggregator=SequenceAttributionAggregator)
+            source_att = agg.source_attributions.numpy()
+            # extract statistics around ;GEND;
+            extract_att = source_att[np.arange(start_idx, end_idx), :]
+            extract_att = extract_att[:,[-4,-3,-2]]     # ignore eos token (no -1)
+            # save for analysis
+            if "FEM" in inputs[i]:
+                extract_atts_fem.append(extract_att)
+            if "MASC" in inputs[i]:
+                extract_atts_masc.append(extract_att)
+            if "NEUT" in inputs[i]:
+                extract_atts_neut.append(extract_att)
+
+    # averaging 
+    # all genders have both correct and inccorect predictions in test set
+    # so not checking for nan values
+    fem_matrix = np.average(extract_atts_fem, axis=0)
+    masc_matrix = np.average(extract_atts_masc, axis=0)
+    neut_matrix = np.average(extract_atts_neut, axis=0)
+    print(f"average attribution score for fem for last 3 tokens: \n{np.around(fem_matrix,2)}\n")
+    print(f"average attribution score for masc for last 3 tokens: \n{np.around(masc_matrix, 2)}\n")
+    print(f"average attribution score for neut for last 3 tokens: \n{np.around(neut_matrix, 2)}\n")
+
+    # average of averages
+    print(f"overall average for all fem suffix: {round(np.mean(fem_matrix), 2)}")
+    print(f"overall average for all masc suffix: {round(np.mean(masc_matrix), 2)}")
+    print(f"overall average for all neut suffix: {round(np.mean(neut_matrix), 2)}")
